c1030985498b0815cb617abb276cd4318e8be3b0
Harry Moreno <morenoh149@gmail.com>
2016-06-24 17:45:08 -0400

    move snippets link to api_root

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from rest_framework import routers
-from tutorial.quickstart import views
+from snippets import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'snippets', views.SnippetViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -27,5 +26,4 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('snippets.urls'))
 ]
